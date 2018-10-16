ce7c8a03ed599ce25009c45b37fbc203d78685d1
karinakozarova <karinakozarova1303@gmail.com>
2018-10-16 12:32:54 +0300

    created new django project

--- docker/startup/run.sh
+++ docker/startup/run.sh
@@ -4,14 +4,8 @@ if [ -n "$GROUP_ID" -a -n "$GROUP_NAME" -a -n "$USER_ID" -a -n "$USER_NAME" ]; t
     adduser -D -u $USER_ID $USER_NAME -G $GROUP_NAME
 fi
 
-[ -e /tmp/worker.pid ] && rm /tmp/worker.pid
-[ -e /tmp/celerybeat.pid ] && rm /tmp/celerybeat.pid
+python /code/eventmanager/manage.py migrate 
+python /code/eventmanager/manage.py collectstatic --no-input
+python /code/eventmanager/manage.py runserver 0.0.0.0:8000  
 
-python /code/webapp/manage.py migrate --settings=webapp.env.local_settings
-python /code/webapp/manage.py loaddata --settings=webapp.env.local_settings initial_user
-python /code/webapp/manage.py collectstatic --no-input
-
-cd /code/webapp && celery multi start -A webapp worker -l info --pidfile=/tmp/worker.pid --concurrency=5 -n worker@%h --logfile="/tmp/%n%I.log"
-cd /code/webapp && celery -A webapp beat -l info -S django --pidfile=/tmp/celerybeat.pid --logfile="/tmp/%n%I.log" --detach
-
-python /code/webapp/manage.py runserver 0.0.0.0:8000 --settings="webapp.env.local_settings"
+tail -f /dev/null
