7ba76a384374ce642f668377f982b4bb8d2cbb2e
karinakozarova <karinakozarova1303@gmail.com>
2018-10-16 11:31:33 +0300

    created docker for project

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,10 @@
 FROM python:alpine3.7
 
-ENV PIP_CACHE_DIR .cache
-
 COPY . /code
 WORKDIR /code
 
 RUN apk update \
     && apk add mariadb-dev \
     g++ bash \
-    && pip3 install wheel -r requirements.txt --target cached-dependencies \
-    && cp -rp cached-dependencies/* /usr/local/lib/python3.6/site-packages/ \
-    && rm -rf cached-dependencies \
+    && pip3 install wheel -r requirements.txt \
     && rm -rf /var/cache/apk/*
--- docker/startup/run.sh
+++ docker/startup/run.sh
@@ -7,11 +7,11 @@ fi
 [ -e /tmp/worker.pid ] && rm /tmp/worker.pid
 [ -e /tmp/celerybeat.pid ] && rm /tmp/celerybeat.pid
 
-python3.6 /code/webapp/manage.py migrate --settings=webapp.env.local_settings
-python3.6 /code/webapp/manage.py loaddata --settings=webapp.env.local_settings initial_user
-python3.6 /code/webapp/manage.py collectstatic --no-input
+python /code/webapp/manage.py migrate --settings=webapp.env.local_settings
+python /code/webapp/manage.py loaddata --settings=webapp.env.local_settings initial_user
+python /code/webapp/manage.py collectstatic --no-input
 
 cd /code/webapp && celery multi start -A webapp worker -l info --pidfile=/tmp/worker.pid --concurrency=5 -n worker@%h --logfile="/tmp/%n%I.log"
 cd /code/webapp && celery -A webapp beat -l info -S django --pidfile=/tmp/celerybeat.pid --logfile="/tmp/%n%I.log" --detach
 
-python3.6 /code/webapp/manage.py runserver 0.0.0.0:8000 --settings="webapp.env.local_settings"
+python /code/webapp/manage.py runserver 0.0.0.0:8000 --settings="webapp.env.local_settings"
