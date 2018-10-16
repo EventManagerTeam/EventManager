43166f7ff17c7cf809a39f4a30438b4382825ffa
Karina Kozarova <karinakozarova1303@gmail.com>
2018-10-16 14:52:39 +0300

    integrate pycodestyle to travis CI

---
+++ eventmanager/tests/test_example.py
@@ -0,0 +1,14 @@
+import unittest
+
+
+class ExampleTestClass(unittest.TestCase):
+    def setUp(self):
+        # Setup run before every test method.
+        pass
+
+    def tearDown(self):
+        # Clean up run after every test method.
+        pass
+
+    def test_something_that_will_pass(self):
+        self.assertFalse(False)
