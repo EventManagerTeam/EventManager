61ec6cb3b334b6bf292b62ae268fb09f4ccd4d7b
karinakozarova1303@gmail.com <karinakozarova1303@gmail.com>
2018-10-29 16:55:37 +0200

    added homepage

--- eventmanager/accounts/views.py
+++ eventmanager/accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def index(request):
-	return render(request, 'templates/accounts/index.html')
+    return render(request, 'accounts/index.html')
