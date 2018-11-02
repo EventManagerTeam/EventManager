29a648842871e811e142f3e296bff08648ec6cf5
Karina Kozarova <karinakozarova1303@gmail.com>
2018-11-02 13:00:36 +0200

    fixed signup urls

--- eventmanager/accounts/views.py
+++ eventmanager/accounts/views.py
@@ -15,7 +15,7 @@ from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
 
 from django.contrib.auth.models import User
-
+from django.contrib.auth import login as auth_login
 
 def index(request):
     return render(request, 'accounts/index.html')
@@ -25,11 +25,11 @@ def login(request):
     if request.method == 'POST':
         form = LoginForm(request.POST)
         if form.is_valid():
-            form.save()
             username = form.cleaned_data.get('username')
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
-            login(request, user)
+            auth_login(request, user)
+            return HttpResponse(User.objects.all())
             return reverse("accounts.index")
     else:
         form = LoginForm()
@@ -48,14 +48,13 @@ def signout(request):
 
 def signup(request):
     if request.method == 'POST':
-        return home(request)
         form = SignUpForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
-            login(request, user)
+            auth_login(request, user)
             return render(request, 'accounts/index.html')
 
     else:
