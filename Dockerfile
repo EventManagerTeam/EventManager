ce269c4b5e5836df04274f1674378e705901b179
Karina Kozarova <karinakozarova1303@gmail.com>
2018-11-02 19:21:26 +0200

    added event model

--- Dockerfile
+++ Dockerfile
@@ -8,3 +8,7 @@ RUN apk update \
     g++ bash \
     && pip3 install wheel -r requirements.txt \
     && rm -rf /var/cache/apk/*
+
+# for Pillow
+RUN apk --update add libxml2-dev libxslt-dev libffi-dev gcc musl-dev libgcc openssl-dev curl
+RUN apk add jpeg-dev zlib-dev freetype-dev lcms2-dev openjpeg-dev tiff-dev tk-dev tcl-dev
--- eventmanager/events/models.py
+++ eventmanager/events/models.py
@@ -1,3 +1,77 @@
-from django.db import models
+import os
 
-# Create your models here.
+from categories.models import Category
+from django.db import models
+from django.utils.translation import ugettext as _
+
+
+def get_image_path(instance, filename):
+    return os.path.join('eventmanager/static/photos', str(instance.id), filename)
+
+
+class Event(models.Model):
+    title = models.CharField(
+        verbose_name=_("Title"),
+        help_text=_("250 character limit"),
+        max_length=250,
+        unique=False,
+        null=False,
+        blank=False
+    )
+
+    description = models.TextField(
+        verbose_name=_("Description"),
+        help_text=_("Plain text, any formatting or links will be removed"),
+        unique=False,
+        null=False,
+        blank=False
+    )
+
+    category = models.ManyToManyField(
+        Category,
+        verbose_name=_("Categories"),
+        help_text=_("Can select many Categories"),
+        blank=False,
+        related_name="categories"
+    )
+
+    cover_image = models.ImageField(
+        upload_to=get_image_path,
+        blank=True,
+        null=True
+    )
+
+    is_active = models.BooleanField(
+        verbose_name=_("Is active"),
+        default=True
+    )
+
+    created_at = models.DateTimeField(
+        verbose_name=_("Created at"),
+        auto_now_add=True
+    )
+
+    updated_at = models.DateTimeField(
+        verbose_name=_("Updated at"),
+        auto_now=True
+    )
+
+    starts_at = models.DateTimeField(
+        verbose_name=_("Event starts at"),
+        blank=True,
+        null=True
+    )
+
+    ends_at = models.DateTimeField(
+        verbose_name=_("Event ends at"),
+        blank=True,
+        null=True
+    )
+
+    class Meta(object):
+        verbose_name = _("Event")
+        verbose_name_plural = _("Events")
+        ordering = ['title']
+
+    def __str__(self):
+        return self.title
