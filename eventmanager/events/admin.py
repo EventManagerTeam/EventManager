90db736dfffd33347050cc03bc87734c4cbaf46b
Karina Kozarova <karinakozarova1303@gmail.com>
2018-11-05 21:12:29 +0200

    list all events

--- eventmanager/events/admin.py
+++ eventmanager/events/admin.py
@@ -1,11 +1,5 @@
 from django.contrib import admin
 from .models import Event
 
-<<<<<<< 1e9f36fa0215f66a5acca90258a573cd39efc4f9
 
 admin.site.register(Event)
-=======
-# from .models import Event
-
-# admin.site.register(Event)
->>>>>>> Feature/23 create event categories and events (#29)
