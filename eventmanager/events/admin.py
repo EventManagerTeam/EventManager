b1f14f55291c575824525158884bb3d645ad55f7
Karina Kozarova <karinakozarova1303@gmail.com>
2018-11-05 18:57:38 +0200

    Feature/23 create event categories and events (#29)
    * added event categories
    
    * fixed setup
    
    * added event model
    
    * trying to fix image saving
    
    * fixed builder
    
    * Update Dockerfile
    
    * upd migration
    
    * fixed pycodestyle problems

--- eventmanager/events/admin.py
+++ eventmanager/events/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from .models import Event
 
+<<<<<<< 1e9f36fa0215f66a5acca90258a573cd39efc4f9
 
 admin.site.register(Event)
+=======
+# from .models import Event
+
+# admin.site.register(Event)
+>>>>>>> Feature/23 create event categories and events (#29)
