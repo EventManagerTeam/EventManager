0e12c5f9862028d0345d87d2ded169024bc1a8df
Karina Kozarova <karinakozarova1303@gmail.com>
2018-11-09 21:17:40 +0200

    addedn change password feature

--- eventmanager/accounts/urls.py
+++ eventmanager/accounts/urls.py
@@ -1,6 +1,7 @@
 from . import views
 from django.contrib.auth import views as auth_views
 from django.urls import path
+from django.conf.urls import url
 
 urlpatterns = [
     path('', views.index, name='accounts.index'),
@@ -8,4 +9,5 @@ urlpatterns = [
     path('signup', views.signup, name='accounts.signup'),
     path('signout', views.signout, name='accounts.signout'),
     path('login', views.login, name='accounts.login'),
+    url(r'^change_password/$', views.change_password, name='change_password'),
 ]
--- eventmanager/accounts/views.py
+++ eventmanager/accounts/views.py
@@ -1,18 +1,25 @@
-from django.shortcuts import redirect
-from django.shortcuts import render
-
-from django.http import HttpResponse
-
-from django.urls import reverse
-
 from accounts.forms import LoginForm
 from accounts.forms import SignUpForm
+
+from django.contrib import messages
 
 from django.contrib.auth import authenticate
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout
+from django.contrib.auth import update_session_auth_hash
+
 from django.contrib.auth.decorators import login_required
+
+from django.contrib.auth.forms import PasswordChangeForm
+
 from django.contrib.auth.models import User
+
+from django.http import HttpResponse
+
+from django.shortcuts import redirect
+from django.shortcuts import render
+
+from django.urls import reverse
 
 
 def index(request):
@@ -59,11 +66,40 @@ def signup(request):
         form = SignUpForm(request.POST)
         if form.is_valid():
             form.save()
+
             username = form.cleaned_data.get('username')
             raw_password = form.cleaned_data.get('password1')
+
             user = authenticate(username=username, password=raw_password)
             auth_login(request, user)
+
             return render(request, 'accounts/index.html')
     else:
         form = SignUpForm()
     return render(request, 'accounts/signup.html', {'form': form})
+
+
+@login_required
+def change_password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)
+
+            success_message = 'Your password was successfully updated!'
+            messages.success(request, success_message)
+
+            return redirect('change_password')
+        else:
+            messages.error(request, 'Please correct the error below.')
+    else:
+        form = PasswordChangeForm(request.user)
+
+    return render(
+        request,
+        'accounts/change_password.html',
+        {
+            'form': form
+        }
+    )
