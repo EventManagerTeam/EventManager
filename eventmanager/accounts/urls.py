445549dcbff6f1402528b186bc48edea47b4cb21
Karina Kozarova <karinakozarova1303@gmail.com>
2018-11-10 16:30:49 +0200

    implemented change of email

--- eventmanager/accounts/urls.py
+++ eventmanager/accounts/urls.py
@@ -1,5 +1,4 @@
 from . import views
-from django.contrib.auth import views as auth_views
 from django.urls import path
 from django.conf.urls import url
 
@@ -10,4 +9,5 @@ urlpatterns = [
     path('signout', views.signout, name='accounts.signout'),
     path('login', views.login, name='accounts.login'),
     url(r'^change_password/$', views.change_password, name='change_password'),
+    url(r'^change_email/$', views.change_email, name='change_email'),
 ]
