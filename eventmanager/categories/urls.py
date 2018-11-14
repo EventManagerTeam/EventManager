9eb2a2c8390be02fd23ed9f3f69467eefd8c9280
Karina Kozarova <karinakozarova1303@gmail.com>
2018-11-14 13:30:38 +0200

    add listing of all categories

--- eventmanager/categories/urls.py
+++ eventmanager/categories/urls.py
@@ -4,4 +4,5 @@ from django.conf.urls import url
 
 urlpatterns = [
     path('all', views.listing, name='categories.listing'),
+    path('<slug:slug>', views.all_from_category, name='categories.all_from_category'),
 ]
--- eventmanager/categories/views.py
+++ eventmanager/categories/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from categories.models import Category
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from events.models import Event
 
 
 def listing(request):
@@ -7,3 +9,21 @@ def listing(request):
     chunks = [categories[x:x + 3] for x in range(0, len(categories), 3)]
     context = {'categories': chunks}
     return render(request, 'categories/all_categories.html', context)
+
+
+def all_from_category(request,slug):
+    category = Category.objects.get(slug=slug)
+    events_list = Event.objects.filter(category=category)
+    number_of_items_per_page = 5
+    paginator = Paginator(events_list, number_of_items_per_page)
+
+    page = request.GET.get('page', 1)
+
+    try:
+        events = paginator.page(page)
+    except PageNotAnInteger:
+        events = paginator.page(1)
+    except EmptyPage:
+        events = paginator.page(paginator.num_pages)
+
+    return render(request, 'events/list_events.html', {'events': events})
