e16ecc1f321afc1cdbe3aa55f784679d1a0fd164
Karina Kozarova <karinakozarova1303@gmail.com>
2018-11-14 12:02:49 +0200

    fixed pycodestyle

--- eventmanager/categories/views.py
+++ eventmanager/categories/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from categories.models import Category
 
+
 def listing(request):
     categories = Category.objects.active()
     chunks = [categories[x:x + 3] for x in range(0, len(categories), 3)]
-    return render(request, 'categories/all_categories.html', {'categories': chunks})
+    context = {'categories': chunks}
+    return render(request, 'categories/all_categories.html', context)
