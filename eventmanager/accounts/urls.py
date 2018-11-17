e05cd516242a0d3b35b8d019e21690327892deb5
Karina Kozarova <karinakozarova1303@gmail.com>
2018-11-18 00:07:06 +0200

    fixed line length

--- eventmanager/accounts/urls.py
+++ eventmanager/accounts/urls.py
@@ -10,6 +10,10 @@ urlpatterns = [
     path('login', views.login, name='accounts.login'),
     path('account', views.account_details, name='accounts.account'),
     url(r'^change_password/$', views.change_password, name='change_password'),
-    url(r'^account_details/$', views.show_account_details, name='accounts.details'),
+    url(
+        r'^account_details/$',
+        views.show_account_details,
+        name='accounts.details'
+    ),
     url(r'^change_email/$', views.change_email, name='change_email'),
 ]
