6c6bd3d63b7361ee2d9f1f450bf4ba106010c979
Karina Kozarova <karinakozarova1303@gmail.com>
2018-11-17 13:20:42 +0200

    created the accountdetails model

--- eventmanager/accounts/models.py
+++ eventmanager/accounts/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.translation import ugettext as _
 
 
 class Accounts(object):
@@ -6,3 +8,35 @@ class Accounts(object):
     def __init__(self, arg):
         super(Accounts, self).__init__()
         self.arg = arg
+
+#TODO add queryset and manager afterwards
+
+
+class AccountDetails(models.Model):
+    user = models.ForeignKey(
+        User,
+        null=True,
+        blank=True,
+        on_delete=models.CASCADE
+    )
+
+    slug = models.SlugField(unique=True, blank=True, null=True)
+
+    profile_picture = models.ImageField(
+        upload_to='profile_pictures',
+        blank=True,
+        null=True
+    )
+    birth_date = models.DateField(("birthdate"),  blank=True, null=True)
+    description = models.TextField(
+        verbose_name=_("Description"),
+        help_text=_("Plain text, any formatting or links will be removed"),
+        unique=False,
+        null=True,
+        blank=True
+    )
+
+    def save_model(self, request, obj, form, change):
+        obj.user = request.user
+        super().save_model(request, obj, form, change)
+
