010226ecb5472da4013ebc066159d7038c683004
Karina Kozarova <karinakozarova1303@gmail.com>
2018-11-30 16:39:15 +0200

    fix imports

--- eventmanager/eventmanager/urls.py
+++ eventmanager/eventmanager/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include
 from django.urls import path
+from django.conf.urls import url
+
 from django.conf.urls.static import static
 from eventmanager import settings
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
