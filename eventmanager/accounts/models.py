bfeea538b2cd6d9602e964a156df8dc28c83dee7
Karina Kozarova <karinakozarova1303@gmail.com>
2018-12-03 21:52:40 +0200

    refactored and added team members - #103 (#106)
    * refactored and added team members - #103
    
    * refactored

--- eventmanager/accounts/models.py
+++ eventmanager/accounts/models.py
@@ -70,6 +70,27 @@ class AccountDetails(models.Model):
             unique_num += 1
         return unique_slug
 
+    def is_my_friend(user, friend):
+        if AccountDetails.objects.filter(user=user).exists():
+            my_details = AccountDetails.objects.get(user=user).friends.all()
+        if AccountDetails.objects.filter(user=friend).exists():
+            friend_details = AccountDetails.objects.get(
+                user=friend).friends.all()
+            if user in friend_details:
+                return True
+            if friend in my_details:
+                return True
+        return False
+
+    def get_my_friends(user):
+        if AccountDetails.objects.filter(user=user).exists():
+            friends = AccountDetails.objects.get(user=user).friends.all()
+
+        for friend in friends:
+            friend.details = AccountDetails.objects.get(user=friend)
+
+        return friends
+
     def save_model(self, request, obj, form, change):
         obj.user = request.user
         if not self.slug:
