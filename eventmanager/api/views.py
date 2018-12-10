16c35d50b379f4c494ee2b325d649514db2473ef
Karina Kozarova <karinakozarova1303@gmail.com>
2018-12-10 14:29:27 +0200

    added api (#130)
    * added api
    * refactored code to follow pycodestyle covnention

---
+++ eventmanager/api/views.py
@@ -0,0 +1,48 @@
+from api.serializers import EventSerializer
+from api.serializers import CategorySerializer
+from api.serializers import CommentsSerializer
+from api.serializers import InvitationsSerializer
+
+from django.core.serializers import serialize
+from django.shortcuts import render
+
+from events.models import Event
+from events.models import Comment
+from events.models import Invite
+from categories.models import Category
+
+from rest_framework import filters
+from rest_framework import viewsets
+from rest_framework.response import Response
+
+
+class EventViewSet(viewsets.ModelViewSet):
+    """
+    A ModelViewSet for listing or retrieving news.
+    """
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+
+
+class CategoriesViewSet(viewsets.ModelViewSet):
+    """
+    A ModelViewSet for listing or retrieving news.
+    """
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class CommentsViewSet(viewsets.ModelViewSet):
+    """
+    A ModelViewSet for listing or retrieving news.
+    """
+    queryset = Comment.objects.all()
+    serializer_class = CommentsSerializer
+
+
+class InvitationsViewSet(viewsets.ModelViewSet):
+    """
+    A ModelViewSet for listing or retrieving news.
+    """
+    queryset = Invite.objects.all()
+    serializer_class = InvitationsSerializer
