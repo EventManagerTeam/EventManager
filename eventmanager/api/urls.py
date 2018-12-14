48269c2e3e9759819572a470564aab31a052b2d4
Karina Kozarova <karinakozarova1303@gmail.com>
2018-12-14 10:54:21 +0200

    added account details to the api

--- eventmanager/api/urls.py
+++ eventmanager/api/urls.py
@@ -4,6 +4,7 @@ from .views import EventViewSet
 from .views import CategoriesViewSet
 from .views import CommentsViewSet
 from .views import InvitationsViewSet
+from .views import AccountDetailsViewSet
 
 
 router = SimpleRouter()
@@ -11,5 +12,6 @@ router.register("events", EventViewSet)
 router.register("comments", CommentsViewSet)
 router.register("categories", CategoriesViewSet)
 router.register("invitations", InvitationsViewSet)
+router.register("details", AccountDetailsViewSet)
 
 urlpatterns = router.urls
