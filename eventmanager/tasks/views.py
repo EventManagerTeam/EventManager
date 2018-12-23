203cdff8e7ee2a9b3a62ffdce093ec3833964d40
Karina Kozarova <karinakozarova1303@gmail.com>
2018-12-23 21:58:49 +0200

    delete task functionality added #115 (#168)
    * delete task functionality added
    
    * improve code

--- eventmanager/tasks/views.py
+++ eventmanager/tasks/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from tasks.models import Task
+from events.models import Event
+import events.views as event_views
+from django.shortcuts import redirect
+from django.urls import reverse
 
-# Create your views here.
+
+def delete_task(request, slug, task):
+    event_id = Event.objects.get(slug=slug).pk
+    Task.objects.filter(event=event_id).get(slug=task).delete()
+    return redirect(reverse('events.board', kwargs={'slug': slug}))
