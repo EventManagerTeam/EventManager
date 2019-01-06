296dc453c1320ccf43749f5205df2e1fe7810088
Karina Kozarova <karinakozarova1303@gmail.com>
2019-01-06 14:03:45 +0200

    Remove useless command from run.sh

--- docker/startup/run.sh
+++ docker/startup/run.sh
@@ -4,7 +4,6 @@ if [ -n "$GROUP_ID" -a -n "$GROUP_NAME" -a -n "$USER_ID" -a -n "$USER_NAME" ]; t
     adduser -D -u $USER_ID $USER_NAME -G $GROUP_NAME
 fi
 
-python /code/eventmanager/manage.py makemigrations
 python /code/eventmanager/manage.py migrate
 python /code/eventmanager/manage.py loaddata /code/eventmanager/initial_categories.json
 python /code/eventmanager/manage.py collectstatic --no-input
