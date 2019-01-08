b102850b65ae5765088ac55a906a26518e884129
Karina Kozarova <karinakozarova1303@gmail.com>
2019-01-08 18:20:08 +0200

    humanize date and refactor event listing card #216

--- eventmanager/eventmanager/settings.py
+++ eventmanager/eventmanager/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'social_django',
+    'django.contrib.humanize',
     'rest_framework',
     'rest_framework_swagger',
     'notifications',
