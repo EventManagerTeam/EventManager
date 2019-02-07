26487edc1c6a46b64c58d037e8154d4b975e1b5e
Karina Kozarova <karinakozarova1303@gmail.com>
2019-02-07 16:31:41 +0200

    added location field

--- eventmanager/eventmanager/settings.py
+++ eventmanager/eventmanager/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'social_django',
+    'django_countries',
     'django.contrib.humanize',
     'rest_framework',
     'rest_framework_swagger',
