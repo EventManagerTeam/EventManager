3d2fb1e49d38313dbb00bf4642a80ea210c1d251
Karina Kozarova <karinakozarova1303@gmail.com>
2019-02-12 21:58:04 +0200

    fix settings

--- eventmanager/eventmanager/settings.py
+++ eventmanager/eventmanager/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.staticfiles',
     'crispy_forms',
-    'django_cleanup.apps.CleanupConfig'
+    'django_cleanup.apps.CleanupConfig',
     'django_countries',
     'notifications',
     'rest_framework',
