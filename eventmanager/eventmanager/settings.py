79caddab8cea61a49c3b7efdddab46a8a68820fb
Karina Kozarova <karinakozarova1303@gmail.com>
2019-02-23 13:33:27 +0200

    use travis env var

--- eventmanager/eventmanager/settings.py
+++ eventmanager/eventmanager/settings.py
@@ -91,16 +91,30 @@ AUTHENTICATION_BACKENDS = (
 
 WSGI_APPLICATION = 'eventmanager.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'eventmanager',
+        'USER': 'eventmanager',
+        'PASSWORD': 'eventmanager',
+        'HOST': 'db',
+        'TEST': {
+            'NAME': 'mytestdatabase',
+        },
     }
 }
+
+
+if os.getenv('TRAVIS', None):    
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+
 
 
 # Password validation
