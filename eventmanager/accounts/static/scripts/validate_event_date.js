3c85ff2ee424e7485e0b76aa9b6894d37306de74
Karina Kozarova <karinakozarova1303@gmail.com>
2019-03-21 22:01:13 +0200

    fixed date restrictions #265

--- eventmanager/accounts/static/scripts/validate_event_date.js
+++ eventmanager/accounts/static/scripts/validate_event_date.js
@@ -1,25 +1,40 @@
 var now = new Date(),
     minDate = now.toISOString().substring(0,10);
+    maxDate = new Date("December 31 9999 00:00");
 
-$('#starts_at').prop('min', minDate);
-$('#ends_at').prop('min', minDate);
+var max_date_message = "Please use a date before December 31 9999 00:00",
+    minDateMessage = "Please use a date in the future";
 
 $("#starts_at").change(function() {
-    starts_at = $('#starts_at').val()
-    if(starts_at < minDate){
-        $(".start_h").text("Please use a date in the future")
+    startsAt = $('#starts_at').val()
+    startsAt = new Date(starts_at);
+
+    if(startsAt < minDate){
+        $(".start_h").text(minDateMessage)
         $(".start_h").removeClass("hidden")
-    } else{
+    } else if(startsAt == "Invalid Date"){
+        console.log(3)
+        $(".start_h").text(max_date_message)
+        $(".start_h").removeClass("hidden")
+    } else {
        $(".start_h").addClass("hidden")  
     }
 });
 
 $("#ends_at").change(function() {
-    ends_at = $('#ends_at').val()
-    if(ends_at < minDate){
-        $(".end_h").text("Please use a date in the future")
+    endsAt = $('#ends_at').val()
+    endsAt = new Date(ends_at);
+
+    if(endsAt < minDate){
+        $(".end_h").text(minDateMessage)
+        $(".end_h").removeClass("hidden")
+    } else if(endsAt == "Invalid Date"){
+        $(".end_h").text(max_date_message)
         $(".end_h").removeClass("hidden")
     } else{
        $(".end_h").addClass("hidden")  
     }
 });
+
+$('#starts_at').prop('min', minDate);
+$('#ends_at').prop('min', minDate);
