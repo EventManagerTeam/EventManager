9d700c47043cc09e93b116664c2b9134daff0513
Karina Kozarova <karinakozarova1303@gmail.com>
2019-04-27 14:22:23 +0300

    rm browse other events button after suggesting category #331

--- eventmanager/categories/views.py
+++ eventmanager/categories/views.py
@@ -15,61 +15,61 @@ from events.models import Event
 
 
 def listing(request):
-    categories = Category.objects.active().sort()
-    chunks = [categories[x:x + 3] for x in range(0, len(categories), 3)]
-    context = {'categories': chunks}
-    return render(request, 'categories/all_categories.html', context)
+	categories = Category.objects.active().sort()
+	chunks = [categories[x:x + 3] for x in range(0, len(categories), 3)]
+	context = {'categories': chunks}
+	return render(request, 'categories/all_categories.html', context)
 
 
 def listing_suggested(request):
-    if SuggestedCategory.objects.count() > 0:
-        categories = SuggestedCategory.objects.all()
-        chunks = [categories[x:x + 3] for x in range(0, len(categories), 3)]
-        context = {'categories': chunks}
-        return render(request, 'categories/all_categories.html', context)
-    else:
-        return render(request, 'categories/no_suggested.html')
+	if SuggestedCategory.objects.count() > 0:
+		categories = SuggestedCategory.objects.all()
+		chunks = [categories[x:x + 3] for x in range(0, len(categories), 3)]
+		context = {'categories': chunks}
+		return render(request, 'categories/all_categories.html', context)
+	else:
+		return render(request, 'categories/no_suggested.html')
 
 
 def all_from_category(request, slug):
-    category = Category.objects.get(slug=slug)
-    events_list = Event.objects.filter(category=category)
-    number_of_items_per_page = 3
-    paginator = Paginator(events_list, number_of_items_per_page)
+	category = Category.objects.get(slug=slug)
+	events_list = Event.objects.filter(category=category)
+	number_of_items_per_page = 3
+	paginator = Paginator(events_list, number_of_items_per_page)
 
-    page = request.GET.get('page', 1)
+	page = request.GET.get('page', 1)
 
-    try:
-        events = paginator.page(page)
-    except PageNotAnInteger:
-        events = paginator.page(1)
-    except EmptyPage:
-        events = paginator.page(paginator.num_pages)
+	try:
+		events = paginator.page(page)
+	except PageNotAnInteger:
+		events = paginator.page(1)
+	except EmptyPage:
+		events = paginator.page(paginator.num_pages)
 
-    categories = Category.objects.sort().active()
-    context = {'events': events, 'categories': categories}
-    return render(request, 'events/list_events.html', context)
+	categories = Category.objects.sort().active()
+	context = {'events': events, 'categories': categories}
+	return render(request, 'events/list_events.html', context)
 
 
 @login_required(login_url='/login')
 def suggest_category(request):
-    form = SuggestedCategoryForm(request.POST or None, request.FILES)
-    if request.method == 'POST':
-        if form.is_valid():
-            category = form.save(commit=False)
-            category.added_by = request.user
+	form = SuggestedCategoryForm(request.POST or None, request.FILES)
+	if request.method == 'POST':
+		if form.is_valid():
+			category = form.save(commit=False)
+			category.added_by = request.user
 
-            if request.FILES.get('category_picture'):
-                category.category_image = request.FILES.get('category_picture')
+			if request.FILES.get('category_picture'):
+				category.category_image = request.FILES.get('category_picture')
 
-            category.save()
+			category.save()
 
-            context = {'success_message': "suggested new category"}
-            return render(request, 'CRUDops/successfully.html', context)
+			context = {'success_message': "suggested new category", 'exclude_button': True}
+			return render(request, 'CRUDops/successfully.html', context)
 
-    context = {'form': form}
-    return render(
-        request,
-        'categories/suggest_category.html',
-        context
-    )
+	context = {'form': form}
+	return render(
+		request,
+		'categories/suggest_category.html',
+		context
+	)
