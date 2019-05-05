ea1d82991ce0fbb16d3161a01ce0d8134f38a736
Karina Kozarova <karinakozarova1303@gmail.com>
2019-05-06 00:19:57 +0300

    slugify should be excluded when running reports #397

--- docker/helpers/coverage.sh
+++ docker/helpers/coverage.sh
@@ -1,4 +1,4 @@
 cd /code/eventmanager
-coverage run manage.py test  -v 2
-coverage report -m --omit eventmanager/slugify.sh
+coverage run manage.py test -v 2
+coverage report -m --omit eventmanager/slugify.py
 coverage html
