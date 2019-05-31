57daeeea361f0f0429b48882985b08f00c58e7c0
Karina Kozarova <karinakozarova1303@gmail.com>
2019-05-31 14:22:17 +0300

    fixed account details when user created by admin

--- eventmanager/accounts/admin.py
+++ eventmanager/accounts/admin.py
@@ -4,6 +4,21 @@ from django.contrib import admin
 from .models import AccountDetails
 from .models import FriendRequest
 
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+
+admin.site.unregister(User)
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    def save_model(self, request, obj, form, change):
+        super().save_model(request, obj, form, change)
+        details = AccountDetails.objects.create(
+            user=obj
+        )
+        details.save()
+
 
 class AccountDetailsAdmin(admin.ModelAdmin):
     list_display = ['slug', 'get_friends', 'birth_date', 'description']
